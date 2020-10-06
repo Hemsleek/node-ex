ee727a351654970e8ce8b1e630935f7e659be983
hemsleek <asiyanbimubashir@gmail.com>
2020-10-06 11:09:44 +0100

    feat: json content

---
+++ index.js
@@ -0,0 +1,32 @@
+//basic http server
+const http = require('http'); //require === import
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2019-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2019-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2019-05-30T19:20:14.298Z",
+      important: true
+    }
+  ]
+
+const app = http.createServer((request , response) =>{
+    response.writeHead(200 , {'content-Type': 'application/json'}) //important
+    response.end(JSON.stringify(notes))
+})
+const PORT =3001;
+
+app.listen(PORT )
+
+console.log('App is running on port', PORT)
