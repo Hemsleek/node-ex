a6bd1393054f047ee4e7442f4fd4091667c6f8ec
hemsleek <asiyanbimubashir@gmail.com>
2020-10-08 12:18:51 +0100

    update: morgan package

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const express = require('express')
 const cors =require('cors')
 
+const morgan = require('morgan')
+
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req[content]'))
 app.disable('x-powered-by')
 app.use(cors())
 
